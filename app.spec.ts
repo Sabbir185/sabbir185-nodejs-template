68617e3afb66174cd72f40dc4098bed90475acc6
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2024-09-01 17:23:40 +0600

    jest, superset

--- app.spec.ts
+++ app.spec.ts
@@ -1,9 +1,17 @@
+import app from "./src/app";
 import { doSum } from "./src/utils";
+import request from "supertest";
 
 describe("App", () => {
     // unit tests
     it("should summary", () => {
         const rr = doSum(2, 3);
         expect(rr).toBe(5);
+    });
+
+    // integration tests
+    it("should should return 200 status code", async () => {
+        const response = await request(app).get("/").send();
+        expect(response.statusCode).toBe(200);
     });
 });
